b3cfa2e8071fcaf373597e4b7093c99b2ccace4a
sami <samisuzano@outlook.com>
2023-12-14 21:21:48 -0300

    backend arrumando

--- src/Controller/alarmesController.js
+++ src/Controller/alarmesController.js
@@ -22,7 +22,7 @@ async alarmesCelulose(request,response){
     const {pagination} = request.body
     const limit = 1000
     const offset = pagination ===1? 0 : pagination*20
-    const data = await bigquery.query(`SELECT alci_cd_identificador,alci_ds_tag,alci_tx_usuario_2,alci_ds_tipo_alarme_1,alci_tx_usuario_1,alci_dt_alarme,alci_dt_final,alci_ds_area FROM ${"`"}sz-00022-ws.ALARMES_E_EVENTOS.ALARMES_CELULOSE${"`"} WHERE alci_ds_origem = "Process(MB300)" AND LOWER(alci_ds_area) LIKE LOWER("%${area}%") AND (alci_dt_alarme>="${dataInicioFormat}" AND alci_dt_alarme<="${dataFimFormat}") ORDER BY alci_cd_identificador DESC LIMIT ${limit} OFFSET ${offset}`);
+    const data = await bigquery.query(`SELECT alci_cd_identificador,alci_ds_tag,alci_tx_usuario_2,alci_ds_tipo_alarme_1,alci_tx_usuario_1,alci_dt_alarme,alci_dt_final,alci_ds_area FROM ${"`"}sz-00022-ws.ALARMES_E_EVENTOS.ALARMES_CELULOSE${"`"} WHERE alci_ds_origem = "Process(MB300)" AND LOWER(alci_ds_area) LIKE LOWER("%${area}%") AND (alci_dt_alarme<="${dataInicioFormat}" OR alci_dt_alarme<="${dataFimFormat}") ORDER BY alci_cd_identificador DESC LIMIT ${limit} OFFSET ${offset}`);
    
     
     return response.status(200).json(data[0])
